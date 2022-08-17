4ee445f2129a7f6a946276b5e0ba25111b571354
achallenger1234 <achallenger1234@gmail.com>
2022-08-17 03:49:28 +0000

    77 commit

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,24 +3,26 @@ import { useCallback, useState } from "react";
 import { useHistory } from "react-router-dom";
 
 import axios from "axios";
-
+import { useMessage } from "./useMessage";
 
 import { User  } from "../types/api/user"
 export const useAuth = () => {
     const [loading, setLoding] = useState(false);
     const history = useHistory();
+    const { showMessage } = useMessage();
     const login = useCallback((id: string) => {
         setLoding(true);
         axios.get<User>(`//jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
             if (res.data) {
+                showMessage({title: "ログインしました", status: "success"})
                 history.push("/home");
             } else {
-                alert('ユーザーが見つかりません');
+                showMessage({title: "ユーザが見つかりません", status: "error"})
             }
-        }).catch( () => alert('ログインできません') )
+        }).catch( () => showMessage({title: "ログインできません", status: "error"}) )
         .finally( () => setLoding(false));
-    }, [history]);
+    }, [history, showMessage]);
     
     return { login, loading };
 }
