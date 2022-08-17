48d22a5712478e68b9bca1b64987e6efd3ffa5da
achallenger1234 <achallenger1234@gmail.com>
2022-08-17 05:48:10 +0000

    79 commit

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,21 +1,38 @@
-import { memo, VFC } from "react";
+import { memo, VFC, useEffect } from "react";
 
-import { Wrap, WrapItem } from "@chakra-ui/react";
+import { Wrap, WrapItem, Spinner, Center } from "@chakra-ui/react";
 
 import { UserCard } from "../organisms/user/UserCard"
+import { useAllUsers } from "../../hooks/useAllUsers"
 
 export const UserManagement: VFC = memo(() => {
+    const { getUsers, loading, users } = useAllUsers();
+    useEffect(() => getUsers(), [])
+    
     return (
-        <Wrap 
-            p={{
-                base: 4,
-                md: 10
-            }}
-        >
-            <WrapItem>
-                <UserCard imageUrl="https://source.unsplash.com/random" userName="じゃけえ" fullName="okamura jak"/>
-            </WrapItem>
-        </Wrap>
+        <>
+            {loading ? (
+                <Center h="100vh">    
+                    <Spinner />
+                </Center>
+                ) : (
+                    <Wrap 
+                        p={{
+                            base: 4,
+                            md: 10
+                        }}
+                    >
+                        {users.map((user) => (
+                            <WrapItem key={user.id} mx="auto">
+                                <UserCard 
+                                    imageUrl="https://source.unsplash.com/random" 
+                                    userName={user.username}
+                                    fullName={user.name}/>
+                            </WrapItem>
+                        ))}
+                    </Wrap>
+                )};
+        </>
         );
 });
 
---
+++ src/hooks/useAllUsers.ts
@@ -0,0 +1,31 @@
+
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { useState, useCallback } from "react";
+import axios from "axios";
+
+import { User } from "../types/api/user"
+import { useMessage  } from "./useMessage"
+
+export const useAllUsers = () => {
+    const [loading, setLoading] = useState(false);
+    const [users, setUsers] = useState<Array<User>>([]);
+    const { showMessage } = useMessage();
+    
+    const getUsers = useCallback(() => {
+        setLoading(true);
+        axios
+            .get<Array<User>>("//jsonplaceholder.typicode.com/users")
+            .then((res) => {setUsers(res.data)})
+            .catch(() => {
+                showMessage({
+                    title: "ユーザ取得に失敗しました",
+                    status: "error"
+                })
+            })
+            .finally(() => {
+                setLoading(false);
+            })
+    }, [])
+    return { getUsers, loading, users }
+}
